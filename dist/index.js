67f3633cd00080f57f49c1bc3882a9934b2bbe43
John Xander <johnblox5@gmail.com>
2022-08-25 20:55:41 +0100

    Objects

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 "use strict";
-let age = 20;
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-let sales: number = 123_456_789;
-let course: string = 'Typescript';
-let is_published: boolean = true;
+// let sales: number = 123_456_789;
+// let course: string = 'Typescript';
+// let is_published: boolean = true;
 
-let numbers: number[] = [1, 2, 3]
+// let numbers: number[] = [1, 2, 3]
+
+// let user: [number, string] = [1, "John"]
+
+// const small = 1
+// const medium = 2
+// const large = 3
+
+// const enum Size { Small = 1, Medium, Large }
+// let mySize: Size = Size.Medium
+// console.log(mySize)
+
+// function calculateTax(income: number, taxYear = 2022): number {
+//     if (taxYear < 2022)
+//         return income * 1.2
+//     return income * 1.3
+// }
+
+// calculateTax(10_000)
+
+let employee: {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = {
+    id: 1,
+    name: "John",
+    retire: (date: Date) => {
+        console.log(date)
+    }
+}
+
